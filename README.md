5e929c1ac45c06d89543c8e005ed7c36ce663886
karan <karankumar46226@gmail.com>
2023-10-25 21:43:02 +0530

    episose-01

---
+++ README.md
@@ -0,0 +1,1 @@
+# Namsate react
---
+++ app.js
@@ -0,0 +1,18 @@
+const parent=React.createElement('div',
+{id:"parent"},
+[React.createElement('div',{id:"child"},
+[React.createElement('h2',{},"I'm an h2 tag"),
+React.createElement('h3',{},'Im an h3 tag')]),
+
+React.createElement('div',{id:"child2"},
+[React.createElement('h2',{},"I'm an h2 tag"),
+React.createElement('h3',{},'Im an h3 tag')])],
+
+
+
+
+)
+
+const root=ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(parent)
