1c7b61bcb5300badc1b51ab49e2e7adbca8d5b1f
karan <karankumar46226@gmail.com>
2023-10-27 12:53:37 +0530

    episode-3

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
 
+const heading=React.createElement('h1',{id:"heading"},"this is an heading tag with namaste react");
+ const root=ReactDOM.createRoot(document.getElementById('root'));
 
-const parent=React.createElement('div',
-{id:"parent"},
-[React.createElement('div',{id:"child"},
-[React.createElement('h2',{},"I'm an h2 tag"),
-React.createElement('h3',{},'Im an h3 tag')]),
+ const jsxHeading=<h1>this is  dhd a jsx bro</h1>
 
-React.createElement('div',{id:"child2"},
-[React.createElement('h2',{},"I'm an h2 tag"),
-React.createElement('h3',{},'Im an h3 tag')])],
+ const fn =() => true
+
+ const fn2=()=>{return true}
+ const num=10000;
+
+ const Heading=()=><h1>Function shorthand Components</h1>
+ const Heading2=()=>(
+ <div>
+    <Heading/>
+    <h1>{num}</h1>
+    {jsxHeading}
+    <h1>Function shorthand  heading2 Components</h1>
+ <h1>Function shorthand  heading2 second time</h1>
+ </div>
+ )
 
 
+ const Heading3=()=>{
+    return <h1>Function Components</h1>
+ }
+ console.log(<Heading/>)
+ root.render(<Heading2/>)
 
-
-)
-
-const root=ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(parent)
